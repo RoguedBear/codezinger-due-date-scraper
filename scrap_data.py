df48e5c97dc156fa3191a52008b98fed39e95c00
RoguedBear <39442192+RoguedBear@users.noreply.github.com>
2022-02-07 19:45:37 +0530

    complete rewrite, data is being scraped successfully

--- scrap_data.py
+++ scrap_data.py
@@ -23,11 +23,13 @@ from typing import List
 
 import selenium.webdriver.remote.webelement
 from pyppeteer import launch
+from pyppeteer.element_handle import ElementHandle
 from pyppeteer.page import Page
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 
 # CONSTANTS
+
 FOLDER_BUTTON_XPATH = "/html/body/app/div[1]/main/ng-component/div[1]/div/problemset-tree/div[1]/div[2]/div/tree-root/tree-viewport/div/div/tree-node-collection/div/tree-node/div/tree-node-wrapper/div/div/div/div/button"
 QUESTIONS_XPATH = "/html/body/app/div[1]/main/ng-component/div[1]/div/problemset-tree/div[1]/div[2]/div/tree-root/tree-viewport/div/div/tree-node-collection/div/tree-node/div/tree-node-children/div/tree-node-collection/div/tree-node/div/tree-node-wrapper/div/div/div"
 QUESTION_TITLE_XPATH = "div[1]/button/span[4]"
@@ -54,16 +56,13 @@ def main(driver: webdriver.Chrome):
 
 
 async def login_codezinger(page: Page, username, password):
-    link = "https://labs.codezinger.com/student/classes/611dc47ba6ae540012d2130f"
+    await set_cookies(page)
+    link = "https://labs.codezinger.com/student/dashboard"
     await page.goto(link)
 
-    if "login" in (await page.title()).lower():
-        await set_cookies(page)
-        await page.goto(link)
-
-        if "login" not in (await page.title()).lower():
-            print("Logged in. allegedly")
-            return
+    if "login" not in (await page.title()).lower():
+        print("Logged in. allegedly")
+        return
 
     if DEBUG or (username and password):
         print("Cookie login didn't work, trying manual login")
@@ -114,36 +113,66 @@ async def expand_all_labs(page: Page):
     print("Expanded", len(buttons), "folders.")
 
 
-def get_data(driver: webdriver.Chrome) -> List[dict]:
+async def sort_pending_by_due_date(page: Page):
+    pending_text_button = []
+    while pending_text_button == []:
+        pending_text_button = await page.xpath(XPATHS["PENDING_DUE_DATE_FILTER"])
+        sleep(0.1)
+    pending_text = await page.evaluate('(node) => node.textContent', pending_text_button[0])
+
+    if pending_text != "Pending By Due Date":
+        await pending_text_button[0].click()
+        due_by_pending_btn = (await page.xpath(XPATHS["PENDING_DUE_DATE_BTN"]))[0]
+        await due_by_pending_btn.click()
+
+
+async def keep_clicking_load_more(page: Page):
+    async def get_load_more() -> list[ElementHandle]:
+        return await page.xpath(XPATHS["LOAD_MORE_BTN"])
+
+    # wait for the button to load first
+    button = []
+    while not button:
+        button = await get_load_more()
+        sleep(0.05)
+
+    while button := await get_load_more():
+        await button[0].click()
+        sleep(0.5)
+
+
+async def get_data(page: Page) -> List[dict]:
+    # TODO: convert this to async
     data_list: List[dict, ...] = []
-    questions = driver.find_elements_by_xpath(QUESTIONS_XPATH)
+    questions = await page.xpath(XPATHS["QUESTION_ROW"])
+    print(questions)
+    print(len(questions))
 
-    print()
-    question: selenium.webdriver.remote.webelement.WebElement
+    question: ElementHandle
     for index, question in enumerate(questions):
-        print("Processing data... ({:3.0%})".format(index/len(questions)), end="\r")
+        print("Processing data... ({:3.0%})".format(index / len(questions)), end="\r")
 
-        problem_no = safe_find_element_by_xpath(question, PROBLEM_NUMBER_XPATH)
-        question_title = safe_find_element_by_xpath(question, QUESTION_TITLE_XPATH)
-        due_date = safe_find_element_by_xpath(question, DUE_DATE_XPATH).rstrip(" /-")
-        status = safe_find_element_by_xpath(question, STATUS_XPATH)
-
+        question_title = await question.xpath(XPATHS["RELATIVE_QUESTION"])
+        codezinger_class = await question.xpath(XPATHS["RELATIVE_CLASS"])
+        question_type = await question.xpath(XPATHS["RELATIVE_Q_TYPE"])
+        due_date = (await question.xpath(XPATHS["RELATIVE_DUE_DATE"]))
         try:
-            parsed_date = datetime.strptime(due_date, "%d %b %I:%M %p")
+            due_date = await page.evaluate('node => node.getAttribute("data-bs-original-title")', due_date[0])
+            due_date = due_date.strip("Due: <br/>Late -")
+            # print(due_date)
+            parsed_date = datetime.strptime(due_date, "%d %b, %I:%M %p")
             parsed_date = parsed_date.replace(year=datetime.now().year)
-            parsed_date = parsed_date.isoformat()
+            # parsed_date = parsed_date.isoformat()
+            # print(index, parsed_date)
         except ValueError:
-            parsed_date = "NULL"
+            parsed_date = datetime.min
 
-        data = {
-            "problem_desc": problem_no + " " + question_title,
-            "assigned_date": "NULL",
-            "submission_date": parsed_date,
-            "status": status == "Submitted" if status != "NULL" else status
-        }
-        data_list.append(data)
-    print()
-
+        data_list.append({
+            "question": await get_text(page, question_title[0]),
+            "class": await get_text(page, codezinger_class[0]),
+            "question_type": "assignment" if (await get_text(page, question_type[0])) else "practise",
+            "due_date": parsed_date
+        })
     print("Scraped", len(questions), "questions")
 
     return data_list
@@ -161,6 +190,11 @@ async def save_cookies(page: Page):
     cookies = await page.cookies()
     with open("cookies.json", "w") as file:
         json.dump(cookies, file)
+
+
+async def get_text(page: Page, element: ElementHandle):
+    return await page.evaluate('(node) => node.textContent', element)
+
 
 def safe_find_element_by_xpath(element: selenium.webdriver.remote.webelement.WebElement, xpath: str) -> str:
     result: str
