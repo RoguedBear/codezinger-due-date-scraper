ffd53e31f51b3edb8bb3d7230f278eac35a2785c
RoguedBear <39442192+RoguedBear@users.noreply.github.com>
2022-02-07 19:58:46 +0530

    nuke selenium

--- main.py
+++ main.py
@@ -14,50 +14,20 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
-import os
-
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.utils import ChromeType
 
 from scrap_data import *
 
-os.environ['WDM_LOCAL'] = '1'
 
-
-def main(driver: webdriver.Chrome):
-    print("A new CHROME browser window will open with the codezinger link in it")
-    print("You have to enter your login and password.")
-    # input("Press enter to continue... ")
-    login_codezinger(driver, "e20cse215@bennett.edu.in", "humpydumpy")
-    expand_all_labs(driver)
-    data = get_data(driver)
-
-
-def driver_exists():
-    import os
-    import sys
-    import webbrowser
-
-    if not os.path.isfile("chromedriver.exe"):
-        print("\033[5;31mYou need to install selenium chromedriver to run this program\033[0m")
-        for i in range(1, 6):
-            print(f"Opening download link in {5 - i}....", end="\r")
-            sleep(1)
-        print()
-        webbrowser.open_new_tab("https://sites.google.com/a/chromium.org/chromedriver/downloads")
-        sys.exit(1)
+def main():
+    print("Hello World")
 
 
 if __name__ == '__main__':
-    # driver_exists()
-    driver = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
     print("""DB-Hax  Copyright (C) 2021  RoguedBear, Ya-s-h
     This program comes with ABSOLUTELY NO WARRANTY; see COPYING
     This is free software, and you are welcome to redistribute it
     under certain conditions; see COPYING""")
     try:
-        main(driver)
+        main()
     except Exception as ex:
         traceback.print_exception(type(ex), ex, ex.__traceback__)
-    finally:
-        driver.close()
